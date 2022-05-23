41fde2a9c569dbbe2246c6795171183eac22993b
itsmattpaw <itsmattpaw@gmail.com>
2022-05-23 16:35:05 -0400

    added coupon model and migration

---
+++ db/migrate/20220523203420_create_coupons.rb
@@ -0,0 +1,16 @@
+class CreateCoupons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :coupons do |t|
+      t.string :name
+      t.string :company
+      t.string :creative_path
+      t.integer :impressions
+      t.integer :clicks
+      t.integer :unique_clicks
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
